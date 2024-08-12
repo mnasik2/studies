543cb7d2fd5cc3382dcff2c6fcec813adb95c1f7
Andrey Mnatsakanyan <mnasik2010@mail.ru>
2024-08-12 22:11:35 +0300

    Спринт 3/19: 1 спринт → Тема 1/5: Базовые алгоритмы → Урок 3/8 2 задание

---
+++ lesson 3 count count if 2nd task.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+bool StartsWithA(const string& x){
+        if(!x.empty()){
+           char first_letter = x[0];
+           return isupper(first_letter);
+        }
+        return false;
+  }
+
+int CountStartsWithA(const vector<string>& xs) {
+    int answer = count_if(xs.begin(), xs.end(), StartsWithA);
+    return answer;
+}
+
+int main() {
+    // не меняйте тело main
+    cout << CountStartsWithA({"And"s, "another"s, "one"s, "gone"s, "another"s,
+                              "one"s
+                              "bites"s,
+                              "the"s, "dust"s});
+
+}
