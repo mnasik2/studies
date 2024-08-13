f04a4953c754a7a6d5bee378eb8b3890a953eb2b
Андрей Мнацаканян <mnasik2010@mail.ru>
2024-08-13 10:56:47 +0300

    Добавлен код Тема 1/5: Базовые алгоритмы → Урок 3/8 задание 2

--- lesson3 count count if 2nd task.cpp
+++ lesson3 count count if 2nd task.cpp
@@ -4,10 +4,10 @@
 #include <algorithm>
 
 using namespace std;
-bool StartsWithA(const string& y){
-        if(!y.empty()){
-           char first_letter = y[0];
-           return isupper(first_letter);
+bool StartsWithA(const string& x){
+        if(!x.empty()){
+           char first_letter = x[0];
+           return isupper(first_letter); //проверка первого символа
         }
         return false;
   }
