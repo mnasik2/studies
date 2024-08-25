7dfc463047f1d2b702d3ed4dce4f78baf921d179
Андрей Мнацаканян <mnasik2010@mail.ru>
2024-08-25 19:43:40 +0300

    Улучшена читаемость кода, согласно рекомендациям.

--- search stroke.cpp
+++ search stroke.cpp
@@ -1,11 +1,12 @@
 #include <algorithm>
+#include <cmath>
 #include <iostream>
+#include <map>
 #include <set>
 #include <string>
 #include <utility>
 #include <vector>
-#include <map>
-#include <cmath>
+
 
 using namespace std;
 
@@ -27,7 +28,9 @@ int ReadLineWithNumber() {
 vector<string> SplitIntoWords(const string& text) {
     vector<string> words;
     string word;
+
     for (const char c : text) {
+
         if (c == ' ') {
             if (!word.empty()) {
                 words.push_back(word);
@@ -37,9 +40,11 @@ vector<string> SplitIntoWords(const string& text) {
             word += c;
         }
     }
+
     if (!word.empty()) {
         words.push_back(word);
     }
+
     return words;
 }
 
@@ -51,11 +56,14 @@ struct Document {
 class SearchServer {
 public:
     void SetStopWords(const string& text) {
+
         for (const string& word : SplitIntoWords(text)) {
+
             if (!word.empty()) {
             stop_words_.insert(word);
         }
     }
+
  }
 
     void AddDocument(int document_id, const string& document) {
@@ -63,11 +71,12 @@ public:
            if (words.empty()) {
             return;
         }
-    const double word_count = static_cast<double>(words.size()); // кол-во слов в векторе
+
+    const double word_count = static_cast<double>(words.size());
     for (const string& word : words) {
-        word_to_document_freqs_[word][document_id] += 1.0 / word_count; //Встретили слово, разделяем сколько раз его встретили, на общее число слов, получаем словарь слов, у каждого из них ID и TF
+        word_to_document_freqs_[word][document_id] += 1.0 / word_count;
     }
-    ++document_count_; // идём по документам
+    ++document_count_; 
 }
 
     vector<Document> FindTopDocuments(const string& raw_query) const {
@@ -82,6 +91,7 @@ public:
         if (matched_documents.size() > MAX_RESULT_DOCUMENT_COUNT) {
             matched_documents.resize(MAX_RESULT_DOCUMENT_COUNT);
         }
+
         return matched_documents;
     }
 
@@ -97,6 +107,7 @@ private:
     vector<string> SplitIntoWordsNoStop(const string& text) const {
         vector<string> words;
         for (const string& word : SplitIntoWords(text)) {
+
             if (!IsStopWord(word)) {
                 words.push_back(word);
             }
@@ -112,40 +123,45 @@ private:
     Query ParseQuery(const string& text) const {
         Query query_words;
         for (const string& word : SplitIntoWords(text)) {
+
             if (!word.empty()) {
+
                 if (word[0] == '-') {
-                    query_words.minus_words.insert(word.substr(1)); // Убираем минус и добавляем в minus_words
+                    query_words.minus_words.insert(word.substr(1)); 
                 } else {
-                    query_words.plus_words.insert(word); // Добавляем в plus_words
+                    query_words.plus_words.insert(word); 
                 }
             }
         }
+
         return query_words;
     }
 
        vector<Document> FindAllDocuments(const Query& query) const {
-        map<int, double> document_to_relevance; //вторую переменную меняем на double
+        map<int, double> document_to_relevance;
 
-        // Обработка плюсовых слов
         for (const string& word : query.plus_words) {
+
             if (word_to_document_freqs_.count(word) > 0) {
+
                 const double idf = log(static_cast<double>(document_count_) / word_to_document_freqs_.at(word).size());
+
                 for (const auto& [document_id, tf] : word_to_document_freqs_.at(word)) {
                     document_to_relevance[document_id] += tf * idf;
                 }
             }
         }
-        // Обработка минусовых слов
         for (const string& word : query.minus_words) {
+
             if (word_to_document_freqs_.count(word) > 0) {
-                for (const auto& [document_id, ignore] : word_to_document_freqs_.at(word)) { //второй элемент нам не нужен в учёте минус-слов
+
+                for (const auto& [document_id, ignore] : word_to_document_freqs_.at(word)) {
                     document_to_relevance.erase(document_id);
                 }
             }
         }
-
-        // теперь несём всё в вектор
         vector<Document> matched_documents;
+
         for (const auto& [document_id, relevance] : document_to_relevance) {
         matched_documents.push_back({document_id, relevance});
     }
@@ -167,11 +183,11 @@ SearchServer CreateSearchServer() {
 }
 
 int main() {
-    const SearchServer search_server = CreateSearchServer();
 
+    const SearchServer search_server = CreateSearchServer();
     const string query = ReadLine();
+
     for (const auto& [document_id, relevance] : search_server.FindTopDocuments(query)) {
-        cout << "{ document_id = " << document_id << ", "
-             << "relevance = " << relevance << " }" << endl;
+        cout << "{ document_id = " << document_id << ", "<< "relevance = " << relevance << " }" << endl;
     }
 }
