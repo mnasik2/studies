6d2c5f537e2a4a31cff8de99d2316bd42ec48d66
Андрей Мнацаканян <mnasik2010@mail.ru>
2024-11-17 21:58:14 +0300

    Обнолён код, добавлены assert

--- codes/test6.cpp
+++ codes/test6.cpp
@@ -1,76 +1,18 @@
-#include <cassert>
-#include <cstdlib>
-#include <utility>
+#include <vector>
+#include <iostream>
 
-template <typename Type>
-class ArrayPtr {
-public:
-    // Инициализирует ArrayPtr нулевым указателем
-    ArrayPtr() = default;
+using namespace std;
 
-    explicit ArrayPtr(size_t size) {
-        if(size == 0){
-        raw_ptr_ = nullptr;
-            }
-        else{
-            raw_ptr_ = new Type[size];
-        }
-    }
-    explicit ArrayPtr(Type* raw_ptr) noexcept : raw_ptr_(raw_ptr) {}
-
-    ArrayPtr(const ArrayPtr&) = delete;
-
-    ~ArrayPtr() {
-        delete[] raw_ptr_;
-    }
-
-    ArrayPtr& operator=(const ArrayPtr&) = delete;
-
-    [[nodiscard]] Type* Release() noexcept {
-        Type* old_ptr = raw_ptr_;
-        raw_ptr_ = nullptr;
-        return old_ptr;
-    }
-
-    Type& operator[](size_t index) noexcept {
-        return raw_ptr_[index];
-    }
-
-    const Type& operator[](size_t index) const noexcept {
-        return raw_ptr_[index];
-    }
-
-    explicit operator bool() const {
-        return raw_ptr_ != nullptr;
-    }
-
-    Type* Get() const noexcept {
-       return raw_ptr_;
-    }
-
-    void swap(ArrayPtr& other) noexcept {
-        std::swap(raw_ptr_, other.raw_ptr_);
-    }
-
-private:
-    Type* raw_ptr_ = nullptr;
-};
-
-int main() {
-    ArrayPtr<int> numbers(10);
-    const auto& const_numbers = numbers;
-
-    numbers[2] = 42;
-    assert(const_numbers[2] == 42);
-    assert(&const_numbers[2] == &numbers[2]);
-
-    assert(numbers.Get() == &numbers[0]);
-
-    ArrayPtr<int> numbers_2(5);
-    numbers_2[2] = 43;
-
-    numbers.swap(numbers_2);
-
-    assert(numbers_2[2] == 42);
-    assert(numbers[2] == 43);
+int main(){
+vector<int> v = {1, 2, 3};
+v.push_back(4);
+const int* data = v.data();
+for (size_t i = 0; i < v.size(); ++i) {
+    cout << *(data + i) << ' ';
+}
+cout << endl;
+for (size_t i = 0; i < v.capacity(); ++i) {
+    cout << *(data + i) << ' ';
+} 
+cout << endl;
 }
